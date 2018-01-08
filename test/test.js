080097f338f6a8705f179dfc0bef846d0d4ca84f
shir-a <shira8948@gmail.com>
2018-01-08 17:49:55 +0200

    red-shiraed

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require("assert"); // node.js core module
-
+var C = require('../cash.js');  // our module
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
